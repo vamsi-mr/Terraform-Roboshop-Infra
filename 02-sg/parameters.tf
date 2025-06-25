fa211c3e79b867dea268c1c6e87a0d9c9a07eab4
Priyanka <priyanka.rel6@gmail.com>
2025-06-25 16:44:56 +0530

    created bastion host module

--- 02-sg/parameters.tf
+++ 02-sg/parameters.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "frontend_sg_id" {
   type  = "String"
   value = module.frontend.sg_id #sg_id taken from module output
 }
+
+resource "aws_ssm_parameter" "bastion_sg_id" {
+  name  = "/${var.project}/${var.environment}/bastion_sg_id"
+  type  = "String"
+  value = module.bastion.sg_id #sg_id taken from module output
+}
--- 02-sg/variables.tf
+++ 02-sg/variables.tf
@@ -14,3 +14,10 @@ variable "frontend_sg_description" {
   default = "created sg for frontend instance"
 }
 
+variable "bastion_sg_name" {
+  default = "bastion"
+}
+
+variable "bastion_sg_description" {
+  default = "created sg for bastion instance"
+}
