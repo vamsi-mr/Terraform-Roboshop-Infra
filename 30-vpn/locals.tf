fcd746710e592f1456954457a71a9de9c41f916d
Priyanka <priyanka.rel6@gmail.com>
2025-06-26 13:52:43 +0530

    created module for openvpn server

---
+++ 30-vpn/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  ami_id           = data.aws_ami.openvpn.id
+  vpn_sg_id        = data.aws_ssm_parameter.vpn_sg_id.value
+  public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+}
+
+locals {
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    Terraform   = true
+  }
+}
---
+++ 30-vpn/main.tf
@@ -0,0 +1,23 @@
+
+# resource "aws_key_pair" "openvpn" {
+#     key_name = "openvpn"
+#     public_key = file("C:\\Vamsi-Devops\\vamsi.pub")
+# }
+
+
+resource "aws_instance" "vpn" {
+  ami                    = local.ami_id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [local.vpn_sg_id]
+  subnet_id              = local.public_subnet_id
+  #key_name = aws_key_pair.openvpn.key_name
+  key_name  = "Linux-KP"
+  user_data = file("openvpn.sh")
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-vpn"
+    }
+  )
+}
