86cf229fa2f52a9075bba6e127ec23ddea17c94f
Priyanka <priyanka.rel6@gmail.com>
2025-06-30 19:10:57 +0530

    formatting the files for reability

--- 00-vpc/outputs.tf
+++ 00-vpc/outputs.tf
@@ -5,3 +5,6 @@
 # output "public_subnet_ids" {
 #   value = module.vpc.public_subnet_ids
 # }
+
+
+# $ for i in 00-vpc/ 10-sg/ 20-bastion/ 30-vpn/ 40-databases/ 50-backend-alb/ ; do cd $i; terraform apply -auto-approve; cd .. ; done
