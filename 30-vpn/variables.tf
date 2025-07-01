2520c5457821fbdd9a4906befbaf2c135d61bc54
Priyanka <priyanka.rel6@gmail.com>
2025-07-01 22:12:48 +0530

    created dev route53 records for vpn

--- 30-vpn/variables.tf
+++ 30-vpn/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z06734122W0TQFHN7RZBR"
+}
+
+variable "domain_name" {
+  default = "ravada.site"
+}
