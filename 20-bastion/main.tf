1e7aff5ace1a6cc008f9e18e1c1e22bc5ecd1735
Priyanka <priyanka.rel6@gmail.com>
2025-07-08 21:06:32 +0530

    added user data to install terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,21 @@ resource "aws_instance" "bastion" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id              = local.public_subnet_id
+  iam_instance_profile = "TerraformAdmin"
+
+user_data = <<-EOF
+            #!/bin/bash
+            yum install yum-utils -y
+            yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+            yum install terraform -y
+            EOF
+
+# need more for terraform
+root_block_device {
+    volume_size = 30
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+
 
   tags = merge(
     local.common_tags,
@@ -11,3 +26,4 @@ resource "aws_instance" "bastion" {
     }
   )
 }
+
